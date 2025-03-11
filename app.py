0ab10d240e608de0664b787cd05a879248f47df1
TheProfitPilgrim <portfoliowealthcreator@gmail.com>
2025-03-11 15:24:46 +0530

    latest

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ st.title("Mutual Fund Backtesting App")
 
 st.header("Select a Selection System")
 
-st.page_link("pages/reports.py", label="Reports")
-
 st.page_link("pages/ss_1.py", label="Selection System 1")
 
 st.write("(More selection systems will be added here)")
